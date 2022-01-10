d5f8b0b4be4e7bc68d7f2bd326324a95b1f2088f
ngranzow <nate.granzow@gmail.com>
2022-01-09 23:54:07 -0600

    feature highscore done

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,13 +24,9 @@ var fightOrSkip = function() {
             //return true if player wants to leave
             return true;
         }
-
-        if (!confirmSkip) {
-            window.alert("You will return to battle!");
-            return fight();
-           }
     }
-}
+    return false;
+};
 
 var fight = function(enemy) {
         var isPlayerTurn = true;
@@ -159,6 +155,8 @@ var startGame = function(){
     playerInfo.reset();
 
     for(var i = 0; i < enemyInfo.length; i++) {
+        console.log(playerInfo);
+
         if (playerInfo.health > 0) {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i+1));
@@ -169,8 +167,7 @@ var startGame = function(){
             // reset enemy.health before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
 
-            // use debugger to pause script from running and check what's going on at that moment in the code
-            // debugger;
+            console.log(pickedEnemyObj);
 
             // pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
             fight(pickedEnemyObj);
@@ -198,12 +195,22 @@ var startGame = function(){
 
 // function to end the entire game
 var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check local storage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has the new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
